f0286d81064b00b1d2a3b1b87f39649af9e4a426
Gal Yedidovich <84014945+Gal-Yedidovich-iSrc@users.noreply.github.com>
2026-06-05 07:43:56 +0300

    Fixed Ad-Quality dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .library(name: "UnityAds", targets: ["UASPM"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.18.1/UnityAds.zip",
+    .package(url: "https://github.com/ironsource-mobile/Unity-Ad-Quality-Swift-Package", from: "9.2.1"),
   ],
   targets: [
     .target(
